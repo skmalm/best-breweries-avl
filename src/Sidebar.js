bc3caffd3d34a67f20b2d8ab6f4b9ee09d259040
Sebastian-Malm <skmalm@gmail.com>
2018-11-12 21:19:51 -0500

    feat: filtering now mostly works, but there's a bug related to brewery index

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+  state = {
+    breweries: this.props.breweries,
+    listBreweriesToDisplay: this.props.breweries,
+  }
+
+  handleFilterChange = (event) => {
+    if (event.target.value === '') {
+      this.setState({ listBreweriesToDisplay: this.state.breweries });
+    }
+    if (event.target.value === 'local') {
+      this.setState({ listBreweriesToDisplay: this.state.breweries.filter(brewery => brewery.local) });
+    }
+    if (event.target.value === 'food') {
+      this.setState({ listBreweriesToDisplay: this.state.breweries.filter(brewery => brewery.food) });
+    }
+  }
+
   render() {
-    const { breweries, onClickListBrewery } = this.props;
+    const onClickListBrewery = this.props.onClickListBrewery;
     return (
       <nav id="sideBar">
         <h1>Asheville's Top 5 Breweries</h1>
         <label htmlFor="filterMenu">Filter:</label>
-        <select id="filterMenu">
+        <select id="filterMenu" onChange={this.handleFilterChange}>
           <option value="">--Choose filter option--</option>
           <option value="local">Local-only</option>
           <option value="food">Serves food</option>
         </select>
         <section>
           <ul>
-          {breweries.map((brewery, index) => (
+          {this.state.listBreweriesToDisplay.map((brewery, index) => (
             <li
               key={brewery.id}
               className='brewery-list-item'
